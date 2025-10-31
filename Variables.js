d368bd877efdcd4ee2a7eaf2f2639eea9ef998fa
Adrian-medina922 <scoezzt@gmail.com>
2025-10-31 14:10:23 +0100

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,15 @@
+
+let nombre=prompt("¿como te llamas?");
+let anoNacimiento=prompt("¿En que año naciste?");
+let anos=2025-anoNacimiento;
+// let anos=2025-"2008";//
+// hemos estado trabajando con variables //
+if(anos>=18){
+    alert("eres mayor de edad");
+}
+else{
+    alert("Eres un chiquito/a");
+    location.href="https://escuelailucla.com/"
+}
+
+
