fc0cd1dc2b749243d1a4135e48298e004f269f2f
Adrian-medina922 <scoezzt@gmail.com>
2025-11-07 14:27:30 +0100

    Add files via upload

---
+++ let a=1;.js
@@ -0,0 +1,17 @@
+// let a = 1;
+// const pi = 3.1415;
+// let nombre = prompt("¿Cómo te llamas?");
+// alert("hola, " + nombre);
+// let ano = prompt("¿En qué año naciste?");
+// let edad = 2025 - ano;
+// alert("tienes " + edad + " años");
+// if (edad >= 18) {
+//     alert("eres mayor de edad");
+// } else {
+//     alert("eres muy pequeño");
+// }
+
+
+for (let i=0;i<10;i++) {
+    console.log("esta es la vez "+i+" que hago  esto");
+}
